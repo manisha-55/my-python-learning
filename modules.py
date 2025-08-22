104a02ed9402326f19abb005968912e8c553ba1a
manisha-55 <mnpogul@gmail.com>
2025-08-22 22:10:41 +0530

    file handling

---
+++ modules.py
@@ -0,0 +1,12 @@
+# import os.path
+# import time
+# import os
+# while True:
+#     if os.path.exists("voosh.txt"):
+#         with open("voosh.txt") as file:
+#             print(file.read())
+#     else:
+#         print("file does not exists")
+#     time.sleep(10)
+
+
