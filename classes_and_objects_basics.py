a736d52f9d081efc95592ea1b18d5936840b5c8a
manisha-55 <mnpogul@gmail.com>
2025-08-25 15:48:57 +0530

    classes_and_objects_basics.py

---
+++ classes_and_objects_basics.py
@@ -0,0 +1,18 @@
+# class Computer:
+#     def config(self):
+#         print("I5, 16 GB, 1TB")
+#
+# com1= Computer
+# Computer.config(com1)
+
+class Computer:
+    def __init__(self, cpu, ram):
+        self.cpu = cpu
+        self.ram = ram
+    def config(self):
+        print("Config is:",self.cpu, self.ram)
+
+com1= Computer("i5",16)
+com2= Computer("Ryzen3",8)
+com1.config()
+com2.config()
