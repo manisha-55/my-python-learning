d292487dd76bd4e106a7822f7b34d07ec834b603
manisha-55 <mnpogul@gmail.com>
2025-08-26 19:14:56 +0530

    Instance method example

---
+++ methods_in_python.py
@@ -0,0 +1,30 @@
+####### Instance variable and Static Variable.
+class Car:
+    wheels = 4     ########### static variable
+
+    def __init__(self):
+        self.mil = 10
+        self.com = "BMW"
+
+c1= Car()
+c2= Car()
+
+c1.mil = 8
+Car.wheels = 5
+
+print(c1.com, c1.mil, c1.wheels)
+print(c2.com, c2.mil, c2.wheels)
+
+class Student:
+    school = 'Telusko'
+    def __init__(self, m1, m2, m3):
+        self.m1 =m1
+        self.m2 = m2
+        self.m3 = m3
+    def avg(self):
+        return (self.m1+self.m2+self.m3)/3
+
+s1= Student(78, 89, 88)
+s2= Student(67, 89, 84)
+print(s1.avg())
+
