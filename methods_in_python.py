9ccc153fc61eaa7064306ed88ca9c6ddb3a611c6
manisha-55 <mnpogul@gmail.com>
2025-08-26 19:40:11 +0530

    Instance method example and class method

--- methods_in_python.py
+++ methods_in_python.py
@@ -30,3 +30,22 @@ s1= Student(78, 89, 88)
 s2= Student(67, 89, 84)
 print(s1.avg())
 
+################# Class Method Type Example
+class Student:
+    school = 'Telusko'
+    def __init__(self, m1, m2, m3):
+        self.m1 =m1
+        self.m2 = m2
+        self.m3 = m3
+    def avg(self):
+        return (self.m1+self.m2+self.m3)/3
+
+    @classmethod
+    def info(cls):
+        return cls.school
+
+s1= Student(78, 89, 88)
+s2= Student(67, 89, 84)
+print(s1.avg())
+print(s1.info())
+print(Student.info())
