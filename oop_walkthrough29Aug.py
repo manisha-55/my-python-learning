ed77688b6327985cc2ceb321b1eb7e68df14b916
manisha-55 <mnpogul@gmail.com>
2025-08-29 18:31:34 +0530

    OOPs in python walkthrough classes and objects

--- oop_walkthrough29Aug.py
+++ oop_walkthrough29Aug.py
@@ -19,3 +19,20 @@ print(u1.name)
 print(u1.email)
 print(u1.contact())
 
+# 2) __repr__ vs __str__
+# __repr__: unambiguous, for developers (debugging).
+# __str__: readable, for users.
+class Ticket:
+    def __init__(self, id, status):
+        self.id= id
+        self.status= status
+    def __str__(self):
+        return f"Ticket(id={self.id!r}, status={self.status!r})"
+    def __repr__(self):
+        return f"[#{self.id}]{self.status}"
+
+t1 = Ticket(11, "Open")
+print(t1.id, t1.status)
+print(repr(t1)) #dev view
+print(str(t1))  #user view
+
