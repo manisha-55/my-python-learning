ce198eb5a5da21051ecdfdd4c2eb53da43945be9
manisha-55 <mnpogul@gmail.com>
2025-09-11 15:26:58 +0530

    Day 7: Exception Handling

--- day7.py
+++ day7.py
@@ -3,6 +3,8 @@
 # If not handled → program crashes.
 # We handle exceptions using try-except.
 
+####### 1. Multiple except blocks
+# Use multiple excepts to handle different types of errors differently.
 try:
     a=int(input("enter num: "))
     b=int(input("enter denominator: "))
@@ -12,3 +14,16 @@ except ZeroDivisionError:
     print("cannot divided by 0")
 except ValueError:
     print("enter valid numbers")
+
+######## 2. else and finally with try
+# else: runs if no exception occurs.
+# finally: runs always, used for cleanup (closing files, releasing resources).
+try:
+    x=int(input("enter a no: "))
+    print("square:",x*x)
+except ValueError:
+    print("invalid no")
+else:
+    print("no error")
+finally:
+    print("end of thr program")
